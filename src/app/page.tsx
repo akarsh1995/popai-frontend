397942fa55543afdd0f317f5d439f46e8e9875be
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-06-19 21:13:50 +0530

    google auth implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import "./page.module.css";
 import Login from "@/components/Login";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-// localhost:3000
-
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <div style={{ width: "500px", margin: "0 auto", paddingTop: "30px" }}>
       <h3>Login Website</h3>
-      <Login />
+      <Login session={session} />
     </div>
   );
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,52 +1,27 @@
 "use client";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 import { DefaultSession } from "next-auth";
+import { FC } from "react";
 
-// Default Session TYPE
-
-export function UserCard({ user }: { user: DefaultSession["user"] }) {
-  return (
-    <div className="card">
-      <div className="card-body">
-        <p>Current Logged In User</p>
-        <h5 className="card-title">{user?.name}</h5>
-        <p className="card-text">{user?.email}</p>
-      </div>
-    </div>
-  );
-}
-
-export default function Login() {
-  // get session from nextAuth
-  const { data: session } = useSession();
-  // useSession uses React Context
-
-  // if the user exists -> show a Sign Out button and their information
+const Login: FC<{ session: DefaultSession | null }> = ({ session }) => {
   if (session) {
     return (
       <>
-        <button
-          onClick={() => signOut()}
-          type="button"
-          className="btn btn-primary"
-        >
+        <button onClick={() => signOut()} type="button">
           Sign Out of Google
         </button>
-        {/* Pass session info to server component */}
-        <UserCard user={session?.user} />
+        Pass session info to server component
       </>
     );
   } else {
     return (
       <>
-        <button
-          onClick={() => signIn()}
-          type="button"
-          className="btn btn-primary"
-        >
+        <button onClick={() => signIn()} type="button">
           Sign In with Google
         </button>
       </>
     );
   }
-}
+};
+
+export default Login;
